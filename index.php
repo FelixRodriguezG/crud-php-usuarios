6c00f7bc3a1bd5d346aeb4282f474a8a041b1475
FelixRG <correofrg@gmail.com>
2025-02-08 12:12:43 +0100

    Add basic HTML structure for CRUD user form and table

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud User</title>
+</head>
+
+<body>
+    <main>
+        <section>
+            <h1>User Form</h1>
+            <form action="">
+                <label for="name">
+                    <input id="name" type="text" name="name" placeholder="Insert your name">
+                </label>
+                <label for="lastname">
+                    <input id="lastname" type="text" name="lastname" placeholder="Insert your last name">
+                </label>
+                <label for="username">
+                    <input id="username" type="text" name="username" placeholder="Insert your username">
+                </label>
+                <label for="email">
+                    <input id="email" type="text" name="email" placeholder="Insert your email">
+                </label>
+                <label for="passwrod">
+                    <input id="passwrod" type="text" name="password" placeholder="Insert your password">
+                </label>
+            </form>
+        </section>
+        <section>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+
+                        <th><a href="">Edit</a></th>
+                        <th><a href="">Remove</a></th>
+                    
+                </tbody>
+            </table>
+        </section>
+    </main>
+</body>
+
+</html>
